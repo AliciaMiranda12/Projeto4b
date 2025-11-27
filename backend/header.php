ade611f95c5743276aa9a8cb79deb402e18517f8
AliciaMiranda12 <mirandacaly@gmail.com>
2025-11-27 15:15:25 -0300

    pg inicial

--- backend/header.php
+++ backend/header.php
@@ -3,21 +3,21 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Página Inicial</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
   </head>
   <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <a class="dropdown-item" href="#">Perfil</a>
-    <a class="dropdown-item" href="index.html">Home</a>
+    <a class="navbar-brand" href="../frontend/index.html">LOGO</a>
+    <a class="dropdown-item" href="../frontend/perfil.html">Perfil</a>
+    <a class="dropdown-item" href="../frontend/index.html">Home</a>
     <a class="dropdown-item" href="../frontend/postagens.html">Postar</a>
     <a class="dropdown-item" href="../frontend/feed.html">Feed</a>
-    <a class="dropdown-item" href="contato.html">Contato</a>
+    <a class="dropdown-item" href="../frontend/contato.html">Contato</a>
       <form class="d-flex" role="Buscar">
-        <input class="form-control me-1" type="Buscar" placeholder="Insira o nome" aria-label="Buscar"/>
-        <button class="btn btn-outline-success" type="submit">Buscar</button>
+       <input class="me-2" type="Buscar" placeholder="Insira o nome" aria-label="Buscar"/>
+       <a class="dropdown-item" href="../frontend/buscar_usuarios_por_id.html">Buscar</a>
       </form>
     </div>
   </div>
